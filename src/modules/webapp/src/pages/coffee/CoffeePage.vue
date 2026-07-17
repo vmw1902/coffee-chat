019cc560818b22f635952b7057ff869b2c25c1d4
Vic <vicmwestern@gmail.com>
2026-07-17 15:45:06 -0700

    chore: removed grid from coffee page + view (#4)

--- src/modules/webapp/src/pages/coffee/CoffeePage.vue
+++ src/modules/webapp/src/pages/coffee/CoffeePage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid-cols-3 grid-rows-3">
-    <RouterView class="w-full h-full" />
+  <div class="w-full h-full">
+    <RouterView />
   </div>
 </template>
 <script setup lang="ts"></script>
