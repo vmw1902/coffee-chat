cd1cc197e237edf4d0a8351d30441cdc3f8463e0
Jonathan Lee <Leeje4@student.okaloosaschools.com>
2026-07-31 19:43:38 -0500

    Home page basics

--- src/modules/webapp/src/pages/dashboard/views/HomeView.vue
+++ src/modules/webapp/src/pages/dashboard/views/HomeView.vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-content">
+        <h1>The Coffee Shop</h1>
+        <p>Freshly brewed coffee made with passion.</p>
+      </div>
+    </section>
+
+    <section class="about">
+      <div class="box">
+        <h2>About Us</h2>
+
+        <p>
+          Welcome to The coffe Shop here we serve handcrafted coffee made from premium beans roasted
+          to perfection. Whether you're looking for your morning espresso or a relaxing afternoon
+          latte, we've got you covered.
+        </p>
+      </div>
+    </section>
+
+    <section class="contact">
+      <h2>Visit Us</h2>
+
+      <p>📍 420 Coffee Street</p>
+      <p>📞 (248) 434-5508</p>
+      <p>🕒 Open Daily: 7 AM - 8 PM</p>
+    </section>
+  </div>
+</template>
